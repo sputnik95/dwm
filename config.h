0258cf0784c775b5d147e398d2dda52c05340e67
sputnik <kss@disroot.org>
2020-01-06 21:59:21 +0300

    First commit

---
+++ config.h
@@ -0,0 +1,155 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "terminus:lang=ru:size=9" };
+static const char dmenufont[]       = "terminus:lang=ru:size=9";
+static const char col_gray1[]       = "#111111";
+static const char col_gray2[]       = "#222222";
+static const char col_gray3[]       = "#8b8792";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_gray1,  col_cyan  },
+};
+
+/* tagging */
+static const char *tags[] = { "all", "main", "www", "cli", "im", "docs", "media", "rss", "mail" };
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Viewnior",    NULL,       NULL,       0,            1,           -1 },
+	{ "feh",         NULL,       NULL,       0,            1,           -1 },
+	{ "Spacefm",     NULL,       NULL,       1 << 1,       1,           -1 },
+	{ "Firefox",     NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Chromium",    NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Pale moon",   NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Pale moon",   NULL, "Pale Moon Preferences",1 << 2, 1,           -1 },
+	{ "URxvt",       NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "TelegramDesktop",NULL,    NULL,       1 << 4,       0,           -1 },
+	{ "Evince",      NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Zathura",     NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "smplayer",    NULL,       NULL,       1 << 6,       0,           -1 },
+	{ "mpv",         NULL,       NULL,       1 << 6,       0,           -1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+};
+
+/* key definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static char dmenumon[2]          = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]     = { "urxvt", NULL };
+static const char *cmusstop[]    = { "cmus-remote", "-s", NULL };
+static const char *cmusplay[]    = { "cmus-remote", "-u", NULL };
+static const char *cmusnext[]    = { "cmus-remote", "-n", NULL };
+static const char *cmusprev[]    = { "cmus-remote", "-r", NULL };
+static const char *volplus[]     = { "amixer", "set", "Master", "5%+", NULL };
+static const char *volminus[]    = { "amixer", "set", "Master", "5%-", NULL };
+static const char *voltoggle[]   = { "amixer", "set", "Master", "toggle", NULL };
+static const char *screensaver[] = { "i3lockr", "--blur=75", NULL };
+static const char *screenconf[]  = { "lxrandr", NULL };
+static const char *rofimenu[]    = { "rofi", "-show", "combi", NULL };
+static const char *startmenu[]   = { "bash", "/home/aezakmi/Apps/scripts/rofi/rofi-bangs.sh", NULL };
+
+
+#include "movestack.c"
+#include <X11/XF86keysym.h>
+static Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = rofimenu } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_Down,   focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Up,     focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_comma,  incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_period, incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_Left,   setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_Right,  setmfact,       {.f = +0.05} },
+    { MODKEY|ShiftMask,             XK_Down,   movestack,      {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_Up,     movestack,      {.i = -1 } },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY,                       XK_k,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+    { MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+    { MODKEY,                       XK_s,      togglesticky,   {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ 0,                            0x1008ff15, spawn,   {.v = cmusstop } },
+	{ 0,                            0x1008ff14, spawn,   {.v = cmusplay } },
+	{ 0,                            0x1008ff17, spawn,   {.v = cmusnext } },
+	{ 0,                            0x1008ff16, spawn,   {.v = cmusprev } },
+	{ 0,                            0x1008ff13, spawn,   {.v = volplus } },
+	{ 0,                            0x1008ff11, spawn,   {.v = volminus } },
+	{ 0,                            0x1008ff12, spawn,   {.v = voltoggle } },
+	{ 0,                            0x1008ff2d, spawn,   {.v = screensaver } },
+	{ 0,                            0x1008ff59, spawn,   {.v = screenconf } },
+	{ 0,                            0x1008ff41, spawn,   {.v = startmenu } },
+	TAGKEYS(                        XK_1,                      1)
+	TAGKEYS(                        XK_2,                      2)
+	TAGKEYS(                        XK_3,                      3)
+	TAGKEYS(                        XK_4,                      4)
+	TAGKEYS(                        XK_5,                      5)
+	TAGKEYS(                        XK_6,                      6)
+	TAGKEYS(                        XK_7,                      7)
+	TAGKEYS(                        XK_8,                      8)
+	TAGKEYS(                        XK_9,                      9)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
---
+++ dwmstatus.sh
@@ -0,0 +1,196 @@
+#!/bin/bash
+
+FG_DEFAULT='#959595'
+BG_DEFAULT='#101010'
+RED='#b82525'
+YELLOW='#b0a225'
+SEPARATOR='#545454'
+
+print_date() {
+	FORMAT="%R %b %d %a"
+	DATE=`date "+${FORMAT}"`
+	echo -n "${DATE}"
+}
+
+separate() {
+    echo -n "|"
+}
+
+print_bat() {
+    CH_STATUS=`cat /sys/class/power_supply/BAT0/status`
+    if [[ $CH_STATUS == 'Discharging' ]]; then
+        CH_STATUS='dis.'
+    elif [[ $CH_STATUS == 'Charging' ]]; then
+        CH_STATUS='ch.'
+    elif [[ $CH_STATUS == 'Unknown'  || $CH_STATUS == 'Full' ]]; then
+        CH_STATUS='bat.'
+    fi
+    BAT_LEVEL=`cat /sys/class/power_supply/BAT0/capacity`
+    OUTPUT="${CH_STATUS}${BAT_LEVEL}%"
+    if [[ $BAT_LEVEL > '65' && $BAT_LEVEL < '99' && $CH_STATUS == 'dis.' ]]; then
+        OUTPUT="${CH_STATUS}${BAT_LEVEL}%)"
+    fi
+    if [[ $BAT_LEVEL -le '65' && $CH_STATUS == 'dis.' ]]; then
+        OUTPUT="${CH_STATUS}${BAT_LEVEL}%"
+    fi
+    echo -n "${OUTPUT}"
+}
+
+print_temp() {
+    CUR_TEMP=`cat /sys/devices/platform/thinkpad_hwmon/hwmon/hwmon0/temp1_input | rev | cut -c4- | rev`
+    #CUR_LOAD=`top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}'`
+    if [[ $CUR_TEMP > '70' ]]; then
+        CUR_TEMP="${CUR_TEMP}°C"
+    fi
+    OUTPUT="${CUR_TEMP}°C"
+    if [[ $CUR_TEMP > '55' && $CUR_TEMP < '65' ]]; then
+        #OUTPUT="${CUR_LOAD}/^fg(${YELLOW})${CUR_TEMP}C^fg(${DEFAULT_FG})"
+        OUTPUT="${CUR_TEMP}°C"
+    fi
+    if [[ $CUR_TEMP -ge '65' ]]; then
+        OUTPUT="${CUR_TEMP}°C"
+    fi
+    echo -n "${OUTPUT}"
+}
+
+print_vol() {
+    CURRENT_STATE=`amixer get Master | egrep 'Playback.*?\[o' | egrep -o '\[o.+\]'`
+    if [[ $CURRENT_STATE == '[off]' ]]; then
+        echo -n "mute"
+    elif [[ $CURRENT_STATE == '[on]' ]]; then
+        VOL_LEVEL=`amixer get Master | awk '$0~/%/{print $4}' | tr -d '[]'`
+        echo -n "vol.${VOL_LEVEL}"
+    fi
+}
+
+print_layout() {
+    LAYOUT=`xkblayout-state print %s`
+    echo -n "${LAYOUT}"
+}
+
+print_song() {
+    INFO_CMUS=$(cmus-remote -Q 2>/dev/null)
+    if [[ $? -eq 0 ]]; then
+        INFO_TITLE=$(echo "${INFO_CMUS}" | sed -n -e 's/^.*title//p' | head -n 1)
+        INFO_ALBUM=$(echo "${INFO_CMUS}" | sed -n -e 's/^.*album//p' | head -n 1)
+        INFO_ARTIST=$(echo "${INFO_CMUS}" | sed -n -e 's/^.*artist //p' | head -n 1)
+        INFO_SEC_DUR=$(cmus-remote -Q | sed -n -e 's/^duration//p' | cut -c2-)
+        INFO_SEC_POS=$(cmus-remote -Q | sed -n -e 's/^position//p' | cut -c2-)
+        INFO_DUR=$(printf '%d:%02d' $(($INFO_SEC_DUR%3600/60)) $(($INFO_SEC_DUR%60)))
+        INFO_POS=$(printf '%d:%02d' $(($INFO_SEC_POS%3600/60)) $(($INFO_SEC_POS%60)))
+        if [[ "${INFO_ARTIST}" ]] && [[ "${INFO_TITLE}" ]]; then
+            OUT_TEXT="${INFO_ARTIST} -${INFO_TITLE} [${INFO_POS}/${INFO_DUR}] |"
+        elif [[ "${INFO_TITLE}" ]]; then
+            OUT_TEXT="${INFO_TITLE} ^fg(${SEPARATOR})|^fg(${DEFAULT_FG}) "
+        elif [ -z "${INFO_TITLE}" ]; then
+            OUT_TEXT=""
+        fi
+    else
+        OUT_TEXT=""
+    fi
+    echo -n "${OUT_TEXT}"
+}
+
+print_netspeed() {
+    # Get custom IN and OUT labels if provided by command line arguments
+    while [[ $# -gt 1 ]]; do
+        key="$1"
+        case "$key" in 
+            -i|--inlabel)
+                INLABEL="$2"
+                shift;;
+            -o|--outlabel)
+                OUTLABEL="$2"
+                shift;;
+        esac
+        shift
+    done
+    
+    [[ -z "$INLABEL" ]] && INLABEL="IN "
+    [[ -z "$OUTLABEL" ]] && OUTLABEL="OUT "
+    
+    # Use the provided interface, otherwise the device used for the default route.
+    if [[ -z $INTERFACE ]] && [[ -n $BLOCK_INSTANCE ]]; then
+      INTERFACE=$BLOCK_INSTANCE
+    elif [[ -z $INTERFACE ]]; then
+      INTERFACE=$(ip route | awk '/^default/ { print $5 ; exit }')
+    fi
+    
+    # Exit if there is no default route
+    [[ -z "$INTERFACE" ]]
+    
+    TREAT_UNKNOWN_AS_UP=1
+    # Issue #36 compliant.
+    if ! [ -e "/sys/class/net/${INTERFACE}/operstate" ] || \
+        (! [ "$TREAT_UNKNOWN_AS_UP" = "1" ] && 
+        ! [ "`cat /sys/class/net/${INTERFACE}/operstate`" = "up" ])
+    then
+        echo "$INTERFACE down"
+        echo "$INTERFACE down"
+        echo "#FF0000"
+    fi
+    
+    # path to store the old results in
+    path="/dev/shm/$(basename $0)-${INTERFACE}"
+    
+    # grabbing data for each adapter.
+    read rx < "/sys/class/net/${INTERFACE}/statistics/rx_bytes"
+    read tx < "/sys/class/net/${INTERFACE}/statistics/tx_bytes"
+    
+    # get time
+    time=$(date +%s)
+    
+    # write current data if file does not exist. Do not exit, this will cause
+    # problems if this file is sourced instead of executed as another process.
+    if ! [[ -f "${path}" ]]; then
+      echo "${time} ${rx} ${tx}" > "${path}"
+      chmod 0666 "${path}"
+    fi
+    
+    # read previous state and update data storage
+    read old < "${path}"
+    echo "${time} ${rx} ${tx}" > "${path}"
+    
+    # parse old data and calc time passed
+    old=(${old//;/ })
+    time_diff=$(( $time - ${old[0]} ))
+    
+    # sanity check: has a positive amount of time passed
+    [[ "${time_diff}" -gt 0 ]] || exit
+    
+    # calc bytes transferred, and their rate in byte/s
+    rx_diff=$(( $rx - ${old[1]} ))
+    tx_diff=$(( $tx - ${old[2]} ))
+    rx_rate=$(( $rx_diff / $time_diff ))
+    tx_rate=$(( $tx_diff / $time_diff ))
+    
+    # shift by 10 bytes to get KiB/s. If the value is larger than
+    # 1024^2 = 1048576, then display MiB/s instead
+    
+    # incoming
+    echo -n "$INLABEL"
+    rx_kib=$(( $rx_rate >> 10 ))
+    if hash bc 2>/dev/null && [[ "$rx_rate" -gt 1048576 ]]; then
+      printf '%sM' "`echo "scale=1; $rx_kib / 1024" | bc`"
+    else
+      echo -n "${rx_kib}K"
+    fi
+    
+    echo -n " "
+    
+    # outgoing
+    echo -n "$OUTLABEL"
+    tx_kib=$(( $tx_rate >> 10 ))
+    if hash bc 2>/dev/null && [[ "$tx_rate" -gt 1048576 ]]; then
+      printf '%sM |' "`echo "scale=1; $tx_kib / 1024" | bc`"
+    else
+      echo -n "${tx_kib}K |"
+    fi
+}
+
+SLEEP_SEC=1  # set bar_delay = 5 in /etc/spectrwm.conf
+#loops forever outputting a line every SLEEP_SEC secs
+while :; do
+    xsetroot -name "$(print_song) $(print_netspeed) $(print_temp) $(separate) $(print_vol) $(separate) $(print_layout) $(separate) $(print_bat) $(separate) $(print_date)"
+        sleep $SLEEP_SEC
+done
