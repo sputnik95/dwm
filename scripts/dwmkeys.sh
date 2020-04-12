c17708465e1f6c57a52eadfbd562fc4330934103
sputnik <kss@disroot.org>
2020-04-12 11:02:34 +0300

    add cmus keybindings

--- scripts/dwmkeys.sh
+++ scripts/dwmkeys.sh
@@ -2,34 +2,26 @@
 case "$1" in
   "lower")
     amixer set Master 5%- >> /dev/null
-    pkill -SIGTRAP dwmstatus
     ;;
   "raise")
     amixer set Master 5%+ >> /dev/null
-    pkill -SIGTRAP dwmstatus
     ;;
   "mute")
     amixer set Master toggle >> /dev/null
-    pkill -SIGTRAP dwmstatus
     ;;
   "musprev")
     cmus-remote -r
-    pkill -SIGTRAP dwmstatus
     ;;
   "musnext")
     cmus-remote -n
-    pkill -SIGTRAP dwmstatus
     ;;
   "mustoggle")
     cmus-remote -u
-    pkill -SIGTRAP dwmstatus
     ;;
   "musstop")
     cmus-remote -s
-    pkill -SIGTRAP dwmstatus
     ;;
   "update")
-    pkill -SIGTRAP dwmstatus
     ;;
   "brightup")
     sudo xbacklight -inc 5
