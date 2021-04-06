04b993ae9787edb15896839808d6890a0d8d5243
sputnik <kss@disroot.org>
2021-04-06 14:20:51 +0300

    change bar height

--- dwm.c
+++ dwm.c
@@ -1955,7 +1955,7 @@ setup(void)
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = drw->fonts->h;
-	bh = drw->fonts->h + 2;
+	bh = drw->fonts->h + 1;
 	th = bh;
 	updategeom();
 	/* init atoms */
